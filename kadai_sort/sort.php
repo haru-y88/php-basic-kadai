559c95c994a27681570b20a3602028c9487275ed
haru-y88 <sp7.y88y@gmail.com>
2024-08-27 16:06:38 +0900

    Add files via upload

---
+++ kadai_sort/sort.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP基礎編</title>
+</head>
+<body>
+  <p>
+    <?php
+
+    function sort_2way($array, $order) {
+      $nums = [15, 4, 18, 23, 10 ];
+      
+      if ($order) {
+        echo '昇順にソートします。<br>';
+        sort($nums);
+        foreach ($nums as $num) {
+          echo $num . '<br>';
+        }
+      } else {
+        echo '降順にソートします。<br>';
+        rsort($nums);
+        foreach ($nums as $num) {
+          echo $num . '<br>';
+        }
+      }
+    }
+    sort_2way($array, TRUE);
+    sort_2way($array, FALSE);
+    ?>
+  </p>
+</body>
+</html>
